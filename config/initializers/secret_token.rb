fcd8820249ed0db17a7d07c67fb03371a7fdf008
Andrew Guard <andrew.s.guard@gmail.com>
2014-01-24 16:59:51 -0600

    set up for heroku and rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Credcard::Application.config.secret_key_base = 'a77faf8ab496f50b74aac7106859dbc6f3cca45f3f9d98722916e449c11aba6d9a2749c3818bd2586e9e6a38953f53f0cfea87960e98cd8d744425defa369ace'
