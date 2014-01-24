a926d0f5456dd4b9a5c0ab442647b72cb27a1d4d
Andrew Guard <andrew.s.guard@gmail.com>
2014-01-24 17:44:32 -0600

    creditcardcharges controller and home view completed, minus styling

--- app/controllers/creditcardcharge_controller.rb
+++ app/controllers/creditcardcharge_controller.rb
@@ -1,2 +1,9 @@
 class CreditcardchargeController < ApplicationController
+  def home
+    @failed = Creditcardcharge.where(:paid => false)
+
+    @disputed = Creditcardcharge.where(:refunded => true)
+
+    @successful = Creditcardcharge.where(:paid => true, :refunded => false)
+  end
 end
