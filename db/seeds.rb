d4ea9fc363969231d16d2c88ccbae1c2d44e5903
Andrew Guard <andrew.s.guard@gmail.com>
2014-01-24 17:23:39 -0600

    fixed tyop in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ customer4.save
 
 
 
-transaction1              = CreditCardCharge.new
+transaction1              = Creditcardcharge.new
 transaction1.paid         = true
 transaction1.amount       = 43000
 transaction1.currency     = 'usd'
@@ -28,7 +28,7 @@ transaction1.refunded     = false
 transaction1.customer_id  = 1
 transaction1.save
 
-transaction2              = CreditCardCharge.new
+transaction2              = Creditcardcharge.new
 transaction2.paid         = true
 transaction2.amount       = 3700
 transaction2.currency     = 'usd'
@@ -36,7 +36,7 @@ transaction2.refunded     = false
 transaction2.customer_id  = 1
 transaction2.save
 
-transaction3              = CreditCardCharge.new
+transaction3              = Creditcardcharge.new
 transaction3.paid         = true
 transaction3.amount       = 430
 transaction3.currency     = 'usd'
@@ -44,7 +44,7 @@ transaction3.refunded     = false
 transaction3.customer_id  = 1
 transaction3.save
 
-transaction4              = CreditCardCharge.new
+transaction4              = Creditcardcharge.new
 transaction4.paid         = true
 transaction4.amount       = 45467
 transaction4.currency     = 'usd'
@@ -52,7 +52,7 @@ transaction4.refunded     = false
 transaction4.customer_id  = 1
 transaction4.save
 
-transaction5              = CreditCardCharge.new
+transaction5              = Creditcardcharge.new
 transaction5.paid         = true
 transaction5.amount       = 2456
 transaction5.currency     = 'usd'
@@ -60,7 +60,7 @@ transaction5.refunded     = false
 transaction5.customer_id  = 1
 transaction5.save
 
-transaction6              = CreditCardCharge.new
+transaction6              = Creditcardcharge.new
 transaction6.paid         = true
 transaction6.amount       = 4564
 transaction6.currency     = 'usd'
@@ -68,7 +68,7 @@ transaction6.refunded     = false
 transaction6.customer_id  = 2
 transaction6.save
 
-transaction7              = CreditCardCharge.new
+transaction7              = Creditcardcharge.new
 transaction7.paid         = true
 transaction7.amount       = 4256
 transaction7.currency     = 'usd'
@@ -76,7 +76,7 @@ transaction7.refunded     = false
 transaction7.customer_id  = 2
 transaction7.save
 
-transaction8              = CreditCardCharge.new
+transaction8              = Creditcardcharge.new
 transaction8.paid         = true
 transaction8.amount       = 68786
 transaction8.currency     = 'usd'
@@ -84,7 +84,7 @@ transaction8.refunded     = false
 transaction8.customer_id  = 2
 transaction8.save
 
-transaction9              = CreditCardCharge.new
+transaction9              = Creditcardcharge.new
 transaction9.paid         = true
 transaction9.amount       = 6567
 transaction9.currency     = 'usd'
@@ -92,7 +92,7 @@ transaction9.refunded     = false
 transaction9.customer_id  = 3
 transaction9.save
 
-transaction10              = CreditCardCharge.new
+transaction10              = Creditcardcharge.new
 transaction10.paid         = true
 transaction10.amount       = 65456
 transaction10.currency     = 'usd'
@@ -100,7 +100,7 @@ transaction10.refunded     = false
 transaction10.customer_id  = 4
 transaction10.save
 
-transaction11              = CreditCardCharge.new
+transaction11              = Creditcardcharge.new
 transaction11.paid         = false
 transaction11.amount       = 564
 transaction11.currency     = 'usd'
@@ -108,7 +108,7 @@ transaction11.refunded     = false
 transaction11.customer_id  = 3
 transaction11.save
 
-transaction12              = CreditCardCharge.new
+transaction12              = Creditcardcharge.new
 transaction12.paid         = false
 transaction12.amount       = 5676
 transaction12.currency     = 'usd'
@@ -116,7 +116,7 @@ transaction12.refunded     = false
 transaction12.customer_id  = 3
 transaction12.save
 
-transaction13              = CreditCardCharge.new
+transaction13              = Creditcardcharge.new
 transaction13.paid         = false
 transaction13.amount       = 123456
 transaction13.currency     = 'usd'
@@ -124,7 +124,7 @@ transaction13.refunded     = false
 transaction13.customer_id  = 3
 transaction13.save
 
-transaction14              = CreditCardCharge.new
+transaction14              = Creditcardcharge.new
 transaction14.paid         = false
 transaction14.amount       = 334435
 transaction14.currency     = 'usd'
@@ -132,7 +132,7 @@ transaction14.refunded     = false
 transaction14.customer_id  = 4
 transaction14.save
 
-transaction15              = CreditCardCharge.new
+transaction15              = Creditcardcharge.new
 transaction15.paid         = false
 transaction15.amount       = 87686
 transaction15.currency     = 'usd'
@@ -140,7 +140,7 @@ transaction15.refunded     = false
 transaction15.customer_id  = 4
 transaction15.save
 
-transaction16              = CreditCardCharge.new
+transaction16              = Creditcardcharge.new
 transaction16.paid         = true
 transaction16.amount       = 5463
 transaction16.currency     = 'usd'
@@ -148,7 +148,7 @@ transaction16.refunded     = true
 transaction16.customer_id  = 1
 transaction16.save
 
-transaction17              = CreditCardCharge.new
+transaction17              = Creditcardcharge.new
 transaction17.paid         = true
 transaction17.amount       = 645456
 transaction17.currency     = 'usd'
@@ -156,7 +156,7 @@ transaction17.refunded     = true
 transaction17.customer_id  = 1
 transaction17.save
 
-transaction18              = CreditCardCharge.new
+transaction18              = Creditcardcharge.new
 transaction18.paid         = true
 transaction18.amount       = 678987
 transaction18.currency     = 'usd'
@@ -164,7 +164,7 @@ transaction18.refunded     = true
 transaction18.customer_id  = 1
 transaction18.save
 
-transaction19              = CreditCardCharge.new
+transaction19              = Creditcardcharge.new
 transaction19.paid         = true
 transaction19.amount       = 5645
 transaction19.currency     = 'usd'
@@ -172,7 +172,7 @@ transaction19.refunded     = true
 transaction19.customer_id  = 2
 transaction19.save
 
-transaction20              = CreditCardCharge.new
+transaction20              = Creditcardcharge.new
 transaction20.paid         = true
 transaction20.amount       = 863
 transaction20.currency     = 'usd'
