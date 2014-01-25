8c4b80c65afbd1ce8454435748eddfe87fcdf0bf
Andrew Guard <andrew.s.guard@gmail.com>
2014-01-24 18:12:00 -0600

    naming error fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Creditcard::Application.configure do
+Credcard::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Creditcard::Application.configure do
+Credcard::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
