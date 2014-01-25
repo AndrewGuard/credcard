d643918982185a7ea2b6abdecb6b21debc7a4422
Andrew Guard <andrew.s.guard@gmail.com>
2014-01-24 18:35:26 -0600

    included capybara in spec_helper

--- spec/requests/creditcardcharges_spec.rb
+++ spec/requests/creditcardcharges_spec.rb
@@ -1,33 +1,17 @@
 require 'spec_helper'
+describe "Creditcardcharges" do
+  describe "Home Page" do
 
-describe "CreditCardCharges" do
-
-  describe "Home page" do
-
-    it "should have 3 h1" do
+    it "should have three headers" do
       visit '/'
       expect(page).to have_css('h1', :count => 3)
     end
 
-    it "says 'Failed Charges'" do
+    it "should have 10 successful charges" do
       visit '/'
-      expect(page).to have_content('Failed Charges')
+      expect(page).to have_selector(".successful", :count => 10)
     end
 
-    it "says 'Disputed Charges'" do
-      visit '/'
-      expect(page).to have_content('Disputed Charges')
-    end
-
-    it "says 'Successful Charges'" do
-      visit '/'
-      expect(page).to have_content('Successful Charges')
-    end
-
-    it "has 10 successful charges" do
-      visit '/'
-      expect(page).to have_css("ul.successful", :count => 10)
-    end
   end
 end
 
